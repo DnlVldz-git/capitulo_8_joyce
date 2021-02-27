92a62e817f851b960c6ca36da6ead63e702a3a9f
duckfuker69 <dani_wii20@hotmail.com>
2021-02-26 19:51:48 -0600

    Primer commi

---
+++ src/main/java/gameZone4/SecretPhrase2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameZone4;
+
+/**
+ *
+ * @author dani
+ */
+public class SecretPhrase2 {
+    private Frase frase;
+    
+    public SecretPhrase2(){
+        this.frase = new Frase();        
+    }
+    
+    public String getFrase(){
+        return this.frase.getFrase_o();
+    }
+    
+    public Boolean adivinanza(char letra){
+        for (int i = 0; i < this.frase.getFrase().length(); i++) {
+            if (letra==this.frase.getFrase().charAt(i)) {
+                frase.destaparFrase(this.frase.getFrase().charAt(i), i);
+            }
+            if (this.frase.ganar()) {
+                System.out.println("Has ganado");
+                return true;                
+            }
+        }
+        return false;
+    }
+    
+    
+}
+
---
+++ src/main/java/gameZone4/Test.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameZone4;
+
+/**
+ *
+ * @author dani
+ */
+import java.util.Scanner;
+
+/**
+ *
+ * @author dani
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        SecretPhrase2 fr2 = new SecretPhrase2();
+        Scanner leer = new Scanner(System.in);
+
+        do {
+            System.out.println(fr2.getFrase());
+            System.out.println("Introduzca su intento");
+            char resp = leer.nextLine().charAt(0);
+            if (fr2.adivinanza(resp)) {
+                break;
+            }
+            
+        } while (true);
+
+    }
+
+}
+
